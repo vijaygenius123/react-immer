ef702910559d8c1ebab6c87811290410401c3550
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-04-17 08:51:11 +0100

    Add and use immer to refactor make it easier

--- src/gift.ts
+++ src/gift.ts
@@ -1,16 +1,25 @@
+import produce from "immer";
 import {IGiftState} from "./@types/Gift.type";
 
 export function addGift(state: IGiftState, id: string, description: string, image: string): IGiftState {
-    return state.gifts.push({
-        id,
-        description,
-        image,
-        reservedBy: undefined
-    })
+    return produce(state, draft => {
+            draft.gifts.push({
+                id,
+                description,
+                image,
+                reservedBy: undefined
+            })
+        }
+    )
 }
 
 export function toggleReservation(state: IGiftState, giftId: string): IGiftState {
-    let gift = state.gifts.find(gift => gift.id === giftId)
-    gift.reservedBy =
-        gift && gift.reservedBy === undefined ? state.currentUser.id : gift.reservedBy === state.currentUser.id ? undefined : gift.reservedBy
+
+    return produce(state, draft => {
+        const gift = draft.gifts.find(gift => gift.id === giftId)
+        if (gift)
+            gift.reservedBy =
+                gift.reservedBy === undefined ? state.currentUser.id : gift.reservedBy === state.currentUser.id ? undefined : gift.reservedBy
+    })
+
 }
