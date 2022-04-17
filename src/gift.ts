17dd8e6f00b608d1f63464137e249423e5e04287
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-04-17 08:41:47 +0100

    Breaking the logic

--- src/gift.ts
+++ src/gift.ts
@@ -1,31 +1,16 @@
 import {IGiftState} from "./@types/Gift.type";
 
 export function addGift(state: IGiftState, id: string, description: string, image: string): IGiftState {
-    return {
-        ...state,
-        gifts: [
-            ...state.gifts,
-            {
-                id,
-                description,
-                image,
-                reservedBy: undefined
-            }
-        ]
-    }
+    return state.gifts.push({
+        id,
+        description,
+        image,
+        reservedBy: undefined
+    })
 }
 
 export function toggleReservation(state: IGiftState, giftId: string): IGiftState {
-    return {
-        ...state,
-        gifts: [
-            ...state.gifts.map(gift => {
-                if (gift.id !== giftId) return gift
-                return {
-                    ...gift,
-                    reservedBy: gift.reservedBy === undefined ? state.currentUser.id : gift.reservedBy === state.currentUser.id ? undefined : gift.reservedBy
-                }
-            })
-        ]
-    }
+    let gift = state.gifts.find(gift => gift.id === giftId)
+    gift.reservedBy =
+        gift && gift.reservedBy === undefined ? state.currentUser.id : gift.reservedBy === state.currentUser.id ? undefined : gift.reservedBy
 }
