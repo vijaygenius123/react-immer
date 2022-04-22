0146832c20e7cc7efc3b16c5b36dc9d36d5b845a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-04-22 07:43:53 +0100

    Update to use curried version of produce

--- src/gift.ts
+++ src/gift.ts
@@ -3,29 +3,23 @@ import {IGiftState} from "./@types/Gift.type";
 import {allUsers, getCurrentUser} from "./users";
 import defaultGifts from './assets/gifts.json'
 
-export function addGift(state: IGiftState, id: string, description: string, image: string): IGiftState {
-    return produce(state, draft => {
-            draft.gifts.push({
-                id,
-                description,
-                image,
-                reservedBy: undefined
-            })
-        }
-    )
-}
-
-export function toggleReservation(state: IGiftState, giftId: string): IGiftState {
-
-    return produce(state, draft => {
-        const gift = draft.gifts.find(gift => gift.id === giftId)
-        if (gift)
-            gift.reservedBy =
-                gift.reservedBy === undefined ? state.currentUser.id : gift.reservedBy === state.currentUser.id ? undefined : gift.reservedBy
+export const addGift = produce((draft: IGiftState, id: string, description: string, image: string) => {
+    draft.gifts.push({
+        id,
+        description,
+        image,
+        reservedBy: undefined
     })
-}
+})
 
-export function getInitialState(){
+export const toggleReservation = produce((draft: IGiftState, giftId: string) => {
+    const gift = draft.gifts.find(gift => gift.id === giftId)
+    if (gift)
+        gift.reservedBy =
+            gift.reservedBy === undefined ? draft.currentUser.id : gift.reservedBy === draft.currentUser.id ? undefined : gift.reservedBy
+})
+
+export function getInitialState() {
     return {
         users: allUsers,
         currentUser: getCurrentUser(),
